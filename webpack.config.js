497d64626288ad0f984b5713976127b1c549d69e
ZamiYizo21 <71885428+ZamiYizo21@users.noreply.github.com>
2021-01-19 10:39:38 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
-const ESlintPlugin = require('eslint-webpack-plugin')
-
+const EsLintPlugin = require("eslint-webpack-plugin")
 
 module.exports = {
-    // 1. Especificando el archivo index de entrada
-    entry: path.join(__dirname, '/src/index.js') ,
+    // 1. Especificar archivo de entrada que va a tomar webpack para el frontend
+    // Especificando el archivo index en la entrada
+    entry: path.join(__dirname, './src/index.js'),
     // 2. Especificar el archivo de salida
     output: {
         path: path.join(__dirname, '/dist'),
@@ -13,22 +13,36 @@ module.exports = {
     // 3. Folder donde servira
     devServer: {
         contentBase: path.join(__dirname, './dist'),
-        port: 4040,
+        // Cambiar el puerto 8080 en caso de que lo tenga ocupado
+        port: 9080,
+        // va a comprimir lo que necesito
         compress: true,
+        // directorio de distribucion (lo hará desde la raiz)
         publicPath: '/'
     },
-    module : {
+    //4. le diremos que vamos a utilizar un nuevo modulo (babel)
+    module: {
         rules: [
+            //Regla que va a permitir entender a babel
+            // JS se lo va a pasar a babel y babel lo escribira de forma moderna
+            // Todos los archivos JS ahora serán babel
             {
                 test: /\.(js)$/,
+                // queremos que solo busque en la carpeta src
                 exclude: /node_modules/,
-                use : ['babel-loader']
+                use: ['babel-loader']
             },
             {
+                // decirle el termino de los archivos
                 test: /\.css$/,
-                use:['style-loader', 'css-loader']
-            }
+                // Los loaders se ejecutan en el orden en que los acomodamos
+                // primero se ejecuta css-loader
+                // css-loader: lee todos los archivos CSS y los regresa en un string
+                // style-loader: modulo que inyecta los css al documento en una etiqueta style
+                use: ['style-loader', 'css-loader']
+            },
         ]
     },
-    plugins :[new ESlintPlugin()]
+    // 5. le pondremos un plugin para utilizar eslint
+    plugins: [new EsLintPlugin()]
 }
